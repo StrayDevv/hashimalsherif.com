e9efbe171c15f21b537fc71d01821a97b4e191cd
nothashem <hsn76575@gmail.com>
2023-07-03 03:52:03 +0300

    Minor fixes + people
    - minor bug fixes
    - fixed style.css problems
    - added people.html
    - added a function in index.js to randomise the list everytime you reload it.

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 function goToMainPage(fileName) {
     window.location.href = `${fileName}.html`;
 }
+
 function openRandomBlog() {
     var blogs = [
         "DebtandConsumerism.html",
         "FiveThingsEverySuccessfulPersonShouldHave.html"
-      ];
+    ];
     var randomIndex = Math.floor(Math.random() * blogs.length);
     var randomBlog = blogs[randomIndex];
     window.location.href = randomBlog;
 }
+
+function shuffleList() {
+    var list = document.querySelector(".class-text");
+    var items = Array.from(list.getElementsByTagName("li"));
+  
+    for (var i = items.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        list.insertBefore(items[j], items[i]);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    shuffleList();
+});
